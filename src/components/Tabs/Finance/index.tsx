7a928cbd373037698a80b8fb408b241fe5a662a8
rafaelzoucas <rafaelzoucas@gmail.com>
2021-09-08 13:49:23 -0300

    desktop

--- src/components/Tabs/Finance/index.tsx
+++ src/components/Tabs/Finance/index.tsx
@@ -28,64 +28,65 @@ export default function Finance() {
           <button onClick={openModalPay}>Adicionar saldo</button>
         </section>
 
-        <section className={styles.timeline}>
+        <div className={styles.bgTimeline}>
+          <section className={styles.timeline}>
 
-          <div className={styles.timelineItem}>
-            <div className={styles.pendingPayment} />
-
-            <div className={styles.titleDescription}>
-                <strong>Pagamento em análise</strong>
-                <p>Pode demorar até 1 dia útil para ser reconhecido.</p>
-            </div>
-
-            <span>19 Abr 2021</span>
-          </div>
-
-          <div className={styles.timelineItem}>
-            <div className={styles.paymentReceived} />
-
-            <div className={styles.titleDescription}>
-                <strong>Pagamento recebido</strong>
-                <p>Você adicionou R$ 79,00 ao seu saldo. Obrigado!</p>
-            </div>
-
-            <span>19 Abr 2021</span>
-          </div>
-
-          <div className={styles.timelineItem}>
-            <div className={styles.overdueInvoice} />
-
-            <div className={styles.titleDescription}>
-                <strong>Saldo insuficiente</strong>
-                <p>O seu saldo atual é de R$ 0,00. Faça uma recarga para continuar utilizando os nossos serviços.</p>
-            </div>
-
-            <span>19 Abr 2021</span>
-          </div>
-
-          <div className={styles.timelineItem} onClick={openModalInvoiceDetails}>
-              <div className={styles.closeInvoice} />
+            <div className={styles.timelineItem}>
+              <div className={styles.pendingPayment} />
 
               <div className={styles.titleDescription}>
-                  <strong>Crédito baixo</strong>
-                  <p>O seu saldo atual é de R$ 10,00. Não se esqueça de recarregar.</p>
+                  <strong>Pagamento em análise</strong>
+                  <p>Pode demorar até 1 dia útil para ser reconhecido.</p>
               </div>
 
               <span>19 Abr 2021</span>
-          </div>
-
-          <div className={styles.timelineItem}>
-            <FontAwesomeIcon icon="star" />
-
-            <div className={styles.titleDescription}>
-                <strong>Bem vindo à</strong>
-                <p>Helix!</p>
             </div>
-            
-            <span>19 Abr 2021</span>
-          </div>
-        </section>
 
+            <div className={styles.timelineItem}>
+              <div className={styles.paymentReceived} />
+
+              <div className={styles.titleDescription}>
+                  <strong>Pagamento recebido</strong>
+                  <p>Você adicionou R$ 79,00 ao seu saldo. Obrigado!</p>
+              </div>
+
+              <span>19 Abr 2021</span>
+            </div>
+
+            <div className={styles.timelineItem}>
+              <div className={styles.overdueInvoice} />
+
+              <div className={styles.titleDescription}>
+                  <strong>Saldo insuficiente</strong>
+                  <p>O seu saldo atual é de R$ 0,00. Faça uma recarga para continuar utilizando os nossos serviços.</p>
+              </div>
+
+              <span>19 Abr 2021</span>
+            </div>
+
+            <div className={styles.timelineItem} onClick={openModalInvoiceDetails}>
+                <div className={styles.closeInvoice} />
+
+                <div className={styles.titleDescription}>
+                    <strong>Crédito baixo</strong>
+                    <p>O seu saldo atual é de R$ 10,00. Não se esqueça de recarregar.</p>
+                </div>
+
+                <span>19 Abr 2021</span>
+            </div>
+
+            <div className={styles.timelineItem}>
+              <FontAwesomeIcon icon="star" />
+
+              <div className={styles.titleDescription}>
+                  <strong>Bem vindo à</strong>
+                  <p>Helix!</p>
+              </div>
+              
+              <span>19 Abr 2021</span>
+            </div>
+          </section>
+        </div>
       </div>
 
       {isModalPayVisible ? (
